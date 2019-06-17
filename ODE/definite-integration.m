f21f5d914a6d442169c54df8838ba8ce67e91e0e
Brenton Horne <brentonhorne77@gmail.com>
2019-06-17 17:45:18 +1000

    Adding extra comments to def integ script

--- ODE/definite-integration.m
+++ ODE/definite-integration.m
@@ -1,12 +1,19 @@
+# Clear all variables
 clear all
+# Show numbers in a long, yet intuitive form
 format long g
+# Number of collocation points
 N         = 1000;
+# Integration interval
 xx0       = 0;
 xxf       = 50;
+# Vector
 n         = 0:N;
+# Extrema grid
 x         = -cos(pi*n'/N);
 # Transformation to [xx0, xxf]
 xx        = (xxf-xx0)/2*(x+1);
+# Chebyshev polys of the first kind
 T         = cos(acos(x)*n);
 # Now for arrays that do not include the endpoints
 xsub      = x(2:N);
@@ -24,12 +31,18 @@ H(1,:)    = T(1,:);
 F(1)      = 0;
 # coefficient vector
 a         = H\F;
-# solution vector
+# solution to ODE vector
 y         = T*a;
+# definite integral
 y         = y-y(1);
-yexact    = sqrt(pi)*(erf(xx-1/2*i)+erf(xx+1/2*i))/(4*exp(1/4))
+# exact indefinite solution per Wolfram Alpha
+yexact    = sqrt(pi)*(erf(xx-1/2*i)+erf(xx+1/2*i))/(4*exp(1/4));
+# exact definite solution
 yexact    = yexact-yexact(1);
+# error to Chebyshev approximation
 err       = abs(yexact-y);
 errrms    = sqrt(err'*err)
+# y approximated by quadratic integration function
 yquad     = quad("g", 0, inf);
+# error in approximation
 errquad   = abs(yexact(end)-yquad)
