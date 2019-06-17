b50a4176e659b6beee83a2afa631873af29a73b4
Brenton Horne <brentonhorne77@gmail.com>
2019-06-17 17:11:36 +1000

    Give integrator script a more appropriate name, given I've changed the func being integrated

--- ODE/definite-integration.m
+++ ODE/definite-integration.m
@@ -18,8 +18,8 @@ dT        = [-(n.^2).*(-1).^(n); dTsub ; (n).^2];
 # LHS without a (coefficient vector)
 H         = 2/(xxf-xx0)*dT;
 # RHS, essentially what we're trying to integrate
-F         = cos(xx).*exp(-xx);
-# Initial condition
+F         = cos(xx).*exp(-xx.^2);
+# Initial condition; with definite integrals y(xx0) = 0
 H(1,:)    = T(1,:);
 F(1)      = 0;
 # coefficient vector
@@ -27,11 +27,9 @@ a         = H\F;
 # solution vector
 y         = T*a;
 y         = y-y(1);
-yexact    = 1/2*exp(-xx).*(sin(xx)-cos(xx));
+yexact    = sqrt(pi)*(erf(xx-1/2*i)+erf(xx+1/2*i))/(4*exp(1/4))
 yexact    = yexact-yexact(1);
 err       = abs(yexact-y);
 errrms    = sqrt(err'*err)
-plot(xx,y)
-#t         = linspace(0,100,1000001)';
-#yode      = lsode("g", 0, t);
-#yode(end)
+yquad     = quad("g", 0, inf);
+errquad   = abs(yexact(end)-yquad)
--- ODE/g.m
+++ ODE/g.m
@@ -1,3 +1,3 @@
 function dy=g(x)
-  dy(1)=airy(0,x)/(1+x)
+  dy(1)=cos(x)/exp(x^2);
 endfunction
